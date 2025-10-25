855c25fc2dc145bb5f93b692095f855998636a9d
Daniil Sivakov <sivakov.daniil@gmail.com>
2025-10-25 20:37:01 +0300

    add streams solutions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-  // Write your code here
+  const reverseTransform = new Transform({
+    encoding: 'utf8',
+    transform(chunk, callback) {
+      const str = chunk.toString();
+      const reversed = str.split('').reverse().join('');
+      callback(null, reversed);
+    }
+  });
+
+  process.stdin.setEncoding('utf8');
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
